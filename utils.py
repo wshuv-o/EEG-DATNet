7c1dc02de97bb62e81e56a4d5d96122fd427576b
Md Wahiduzzaman Suva <wshuvo360@gmail.com>
2025-05-09 01:47:39 +0600

    updated the utility function

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import cv2
 import os
 import torch
 import numpy as np
+import numpy as np
+from matplotlib import pyplot as plt
+
 
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters() if p.requires_grad)
@@ -173,3 +176,65 @@ class GradCAM:
             print(
                 f"An exception occurred in CAM with block: {exc_type}. Message: {exc_value}")
             return True
+
+
+class MotorImageryDataset:
+    def __init__(self, dataset='A01T.npz'):
+        if not dataset.endswith('.npz'):
+            dataset += '.npz'
+
+        self.data = np.load(dataset)
+
+        self.Fs = 250  # 250Hz from original paper
+
+        # keys of data ['s', 'etyp', 'epos', 'edur', 'artifacts']
+
+        self.raw = self.data['s'].T
+        self.events_type = self.data['etyp'].T
+        self.events_position = self.data['epos'].T
+        self.events_duration = self.data['edur'].T
+        self.artifacts = self.data['artifacts'].T
+
+        # Types of motor imagery
+        self.mi_types = {769: 'left', 770: 'right',
+                         771: 'foot', 772: 'tongue', 783: 'unknown'}
+
+    def get_trials_from_channel(self, channel=7):
+
+        # Channel default is C3
+
+        startrial_code = 768
+        starttrial_events = self.events_type == startrial_code
+        idxs = [i for i, x in enumerate(starttrial_events[0]) if x]
+
+        trials = []
+        classes = []
+
+        for index in idxs:
+            try:
+                type_e = self.events_type[0, index+1]
+                class_e = self.mi_types[type_e]
+                classes.append(class_e)
+
+                start = self.events_position[0, index]
+                stop = start + self.events_duration[0, index]
+                trial = self.raw[channel, start:stop]
+                trial = trial.reshape((1, -1))
+                trials.append(trial)
+
+            except:
+                continue
+
+        return trials, classes
+
+    def get_trials_from_channels(self, channels=[7, 9, 11]):
+        trials_c = []
+        classes_c = []
+        for c in channels:
+            t, c = self.get_trials_from_channel(channel=c)
+
+            tt = np.concatenate(t, axis=0)
+            trials_c.append(tt)
+            classes_c.append(c)
+
+        return trials_c, classes_c
